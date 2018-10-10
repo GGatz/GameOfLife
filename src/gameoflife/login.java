74e3d81f4e310e835ecd3e531a59143ec9a2c46f
GGatz <treclifton@gmail.com>
2018-10-10 00:01:10 -0500

    did stuff

---
+++ src/gameoflife/login.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameoflife;
+
+/**
+ *
+ * @author tre
+ */
+public class login {
+
+    String name, password;
+    
+    
+    public boolean checkLogin(String name, String password)
+    {
+        if((name == this.name) && (password == this.password))
+        {
+            return true;
+        }
+        return false;
+    }
+}
