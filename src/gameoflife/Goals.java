8bfe2cc318dd93c66df14e0fe91b8b166d5dbd0b
GGatz <treclifton@gmail.com>
2018-10-10 00:30:55 -0500

    added some initial methods and fields

---
+++ src/gameoflife/Goals.java
@@ -0,0 +1,22 @@
+package gameoflife;
+
+/**
+ *
+ * @author Gatsby
+ */
+public class Goals {
+    void completeGoal()
+    {
+        //TODO: add code for the competion of goals
+    }
+    
+    void abandonGoal()
+    {
+        //TODO: add code for removing of goals that have already been accepted
+    }
+    
+    void acceptNewGoal()
+    {
+        //TODO: add code for accepting new goals
+    }
+}
